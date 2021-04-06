fafa1afbbff76a9f7fb37009d4c4fdd90d0f9391
cloud <62519659+respecting@users.noreply.github.com>
2021-04-05 20:08:49 -0600

    bump version

--- NitroPerks.plugin.js
+++ NitroPerks.plugin.js
@@ -36,7 +36,7 @@ module.exports = (() => {
                 "discord_id": "407348579376693260",
                 "github_username": "respecting"
             }],
-            "version": "1.3.2",
+            "version": "1.3.3",
             "description": "Unlock all screensharing modes, and use cross-server emotes & gif emotes, Discord wide! (You CANNOT upload 100MB files though. :/)",
             "github": "https://github.com/respecting/NitroPerks",
             "github_raw": "https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js"
